623304b4fa1cbc39233f7fd6d4e95615d9a57027
jeswanth <venkateshjaripiti123@gmail.com>
2026-01-22 02:35:08 +0530

    Auto create admin user on startup

--- VJISS_APP/apps.py
+++ VJISS_APP/apps.py
@@ -1,11 +1,5 @@
 from django.apps import AppConfig
-import os
+
 
 class VjissAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "VJISS_APP"
-
-    def ready(self):
-        if os.environ.get("RENDER"):  # prevents local execution
-            from .bootstrap import bootstrap_admin_user
-            bootstrap_admin_user()
+    name = 'VJISS_APP'
