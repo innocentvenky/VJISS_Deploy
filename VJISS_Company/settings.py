71cc76ac975804c90aad6a61bb6a1d5619abd300
jeswanth <venkateshjaripiti123@gmail.com>
2026-01-23 13:48:46 +0530

    storeage

--- VJISS_Company/settings.py
+++ VJISS_Company/settings.py
@@ -168,8 +168,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL='/media/'
-MEDIA_ROOT=BASE_DIR / 'media'  
+
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
